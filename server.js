9b0cd12084d406c74328d85d59a3c993d12ea45e
hungchihsueh <chihsuehhung@gmail.com>
2021-11-15 13:53:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express')
+require("dotenv").config();
 const app = express()
 const cors = require('cors')
+const PORT= express
 app.use(cors())
 app.use(express.json())
+
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+app.use((req,res)=>{
+    console.log("migga");
+    res.send("koe mama")
+})
+
+
+
+
+
+
 
 app.listen(3001, () => {
     console.log("Server is listening at Port 3001");
