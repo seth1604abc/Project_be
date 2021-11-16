efa1988a0967d63dbd4768800fff82f01ed28c98
Ariel Liu <regina31664@gmail.com>
2021-11-16 22:11:24 +0800

    xxxx

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+app.post("/event"), (req, res) => {
+    const eventInsert = "INSERT INTO event (title, datetime, location, limit, content) VALUES (?,?,?,?,?)" 
+}
+
 app.listen(3001, () => {
     console.log("Server is listening at Port 3001");
 })
