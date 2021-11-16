57152cf3c050d9aa1afff6993fbf92896c02e7b9
seth1604abc <seth1605abc@gmail.com>
2021-11-16 20:58:08 +0800

    login

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors())
-app.use(express.json())
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],    
+        credentials: true,
+    })
+);
+app.use(session({   
+    resave: false,
+    saveUninitialized: false,
+    secret: "123456789",
+    cookie: {        
+        maxAge: 1000 * 60 * 60 * 2, //存在兩小        
+    }
+}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//設置後端路由
+const userRouter = require('./router/User')
+app.use("/auth", userRouter)
+const giftRouter = require('./router/GiftCard')
+app.use("/gift", giftRouter)
+const memberRouter = require('./router/Member')
+app.use("/member", memberRouter)
 
 app.listen(3001, () => {
     console.log("Server is listening at Port 3001");
