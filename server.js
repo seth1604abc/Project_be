4534c6d43de4eb9ecc12b063381242fbd246b4fa
hungchihsueh <chihsuehhung@gmail.com>
2021-11-17 14:20:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,40 @@
-const express = require('express')
+const express = require("express");
 require("dotenv").config();
-const app = express()
-const cors = require('cors')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const app = express();
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { connection } = require("./utilities/db");
+require("dotenv").config();
 
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],    
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
 );
-app.use(session({   
+app.use(
+  session({
     resave: false,
     saveUninitialized: false,
     secret: "123456789",
-    cookie: {        
-        maxAge: 1000 * 60 * 60 * 2, //存在兩小        
-    }
-}))
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2, //存在兩小
+    },
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //設置後端路由
-const userRouter = require('./router/User')
-app.use("/auth", userRouter)
-const giftRouter = require('./router/GiftCard')
-app.use("/gift", giftRouter)
-const memberRouter = require('./router/Member')
-app.use("/member", memberRouter)
-
-
-
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  });
-
-app.use((req,res)=>{
-    console.log("migga");
-    res.send("koe mama")
-})
-
-
-
-
-
-
+const userRouter = require("./router/User");
+app.use("/auth", userRouter);
+const giftRouter = require("./router/GiftCard");
+app.use("/gift", giftRouter);
+const memberRouter = require("./router/Member");
+app.use("/member", memberRouter);
 
 app.listen(3001, () => {
-    console.log("Server is listening at Port 3001");
-})
+  console.log("Server is listening at Port 3001");
+});
