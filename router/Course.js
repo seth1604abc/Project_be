76558f2a82afdf932a5736d9da4a4725575bf0d6
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-11-20 02:03:37 +0800

    2021/11/20 - 不解為啥是result[0]

--- router/Course.js
+++ router/Course.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../utilities/db");
+const moment = require('moment');
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
-  let isUnique = await con.queryAsync("SELECT course.*,body_part.name as body_part_id FROM course JOIN body_part ON course.body_part_id = body_part.id")
-    res.json(isUnique)
+  let result = await con.queryAsync("SELECT course.*,body_part.name as body_part_id FROM course JOIN body_part ON course.body_part_id = body_part.id")
+  if(result) {
+        result[0].upload_time = moment(result[0].upload_time).format('YYYY-MM-DD');
+        result[1].upload_time = moment(result[1].upload_time).format('YYYY-MM-DD');
+    }   
+    res.json(result)
 });
 
 router.get("/:id", async (req, res) => {
