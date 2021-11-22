80a9e20c56508924fbeceafdcd0450f29e50b2fd
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-11-22 21:12:43 +0800

    2021/11/22 - 更新後台

--- router/Course.js
+++ router/Course.js
@@ -1,22 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../utilities/db");
-const moment = require('moment');
+const moment = require("moment");
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
-  let result = await con.queryAsync("SELECT *FROM course")
-  if(result) {
-    for(let i=0; i<result.length; i++){
-      result[i].upload_time = moment(result[i].upload_time).format('YYYY-MM-DD');
+  let result = await con.queryAsync(
+    "SELECT *FROM course ORDER BY upload_time DESC"
+  );
+  if (result) {
+    for (let i = 0; i < result.length; i++) {
+      result[i].upload_time = moment(result[i].upload_time).format(
+        "YYYY-MM-DD"
+      );
     }
-    }   
-    res.json(result)
+  }
+  res.json(result);
 });
 
-router.get("/:id", async (req, res) => {
-  let isUnique = await con.queryAsync("SELECT * FROM course WHERE id = ?",[req.params.id])
-    res.json(isUnique)
+router.get("/hitsort", async (req, res) => {
+  let result = await con.queryAsync(
+    "SELECT *FROM course ORDER BY likes DESC"
+  );
+  if (result) {
+    for (let i = 0; i < result.length; i++) {
+      result[i].upload_time = moment(result[i].upload_time).format(
+        "YYYY-MM-DD"
+      );
+    }
+  }
+  res.json(result);
+});
+
+
+router.get("/SingleCourse/:id", async (req, res) => {
+  let result = await con.queryAsync("SELECT * FROM course WHERE id = ?", [
+    req.params.id,
+  ]);
+  res.json(result);
 });
 
 module.exports = router;
