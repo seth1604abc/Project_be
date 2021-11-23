d56a36433c4c7a1511dd4f7a1bc733fe1e9332a4
hungchihsueh <chihsuehhung@gmail.com>
2021-11-23 14:55:43 +0800

    Update Product.js

--- router/Product.js
+++ router/Product.js
@@ -84,51 +84,6 @@ router.get("/equipment", async (req, res) => {
   res.json(result);
 });
 
-//取得健身器材-手部
-router.get("/equipment/hand", async (req, res) => {
-  let id = req.session.userId;
-  let result = await con.queryAsync(
-    "SELECT * FROM product WHERE product_type_id=3 AND body_part_id=1"
-  );
-  res.json(result);
-});
-
-//取得健身器材-肩部
-router.get("/equipment/shoulder", async (req, res) => {
-  let id = req.session.userId;
-  let result = await con.queryAsync(
-    "SELECT * FROM product WHERE product_type_id=3 AND body_part_id=2"
-  );
-  res.json(result);
-});
-
-//取得健身器材-胸部
-router.get("/equipment/chest", async (req, res) => {
-  let id = req.session.userId;
-  let result = await con.queryAsync(
-    "SELECT * FROM product WHERE product_type_id=3 AND body_part_id=3"
-  );
-  res.json(result);
-});
-
-//取得健身器材-背部
-router.get("/equipment/back", async (req, res) => {
-  let id = req.session.userId;
-  let result = await con.queryAsync(
-    "SELECT * FROM product WHERE product_type_id=3 AND body_part_id=4"
-  );
-  res.json(result);
-});
-
-//取得健身器材-腿部
-router.get("/equipment/leg", async (req, res) => {
-  let id = req.session.userId;
-  let result = await con.queryAsync(
-    "SELECT * FROM product WHERE product_type_id=3 AND body_part_id=5"
-  );
-  res.json(result);
-});
-
 //商品單獨頁
 router.get("/single/:id",async(req,res)=>{
   let id= req.params.id
