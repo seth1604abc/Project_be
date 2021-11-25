55acf7085fe8d8e4187e2aeb5a1300d4627c7e3f
hungchihsueh <chihsuehhung@gmail.com>
2021-11-25 16:44:33 +0800

    Update Cart.js

--- router/Cart.js
+++ router/Cart.js
@@ -3,10 +3,12 @@ const con = require("../utilities/db");
 const router = express.Router();
 
 //加入購物車
-router.post("/addcart/:userId/:productId/:amount", async (req, res) => {
+router.post("/addcart/:productId/", async (req, res) => {
     let id = req.session.userId;
+    let data = req.body;
+    console.log(data);   
     let result = await con.queryAsync(
-      "INSERT INTO cart(user_id,product_id,amount) VALUES(?)",[[req.params.userId,req.params.productId,req.params.amount]]
+      "INSERT INTO cart(user_id,product_id,amount) VALUES(?)",[[1,req.params.productId,data.number]]
     );
     res.json(result);
   });
