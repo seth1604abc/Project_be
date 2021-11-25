0be5e43bbee005316f6727e78eeef25d2d123db0
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-11-25 21:09:12 +0800

    2021/11/25 - 新曾子留言的路由

--- router/Course.js
+++ router/Course.js
@@ -40,4 +40,31 @@ router.get("/SingleCourse/:id", async (req, res) => {
   res.json(result);
 });
 
+router.get("/comment", async (req, res) => {
+  let mainComment = await con.queryAsync("SELECT course_comment.*, user.first_name AS user_id ,user.image FROM course_comment JOIN user ON course_comment.user_id=user.id");
+  let sonComment = await con.queryAsync("SELECT response_comment.*, user.first_name AS user_id ,user.image FROM response_comment JOIN user ON response_comment.user_id=user.id");
+  let allComment = [...mainComment,sonComment]
+  res.json(allComment);
+});
+
+router.post("/addComment", async (req, res) => {
+  let text =req.body.text
+  let user_id =req.body.user_id
+  let course_id =req.body.course_id
+  let created_at =req.body.created_at
+  console.log(req.body)
+  let addComment = await con.queryAsync("INSERT INTO course_comment (user_id,content,course_id,created_at) VALUES (?,?,?,?)",[user_id,text,course_id,created_at]);
+  res.json('有了');
+});
+
+router.post("/addChildrenComment", async (req, res) => {
+  let text =req.body.text
+  let user_id =req.body.user_id
+  let course_comment_id =req.body.course_comment_id
+  let created_at =req.body.created_at
+  console.log(req.body)
+  let addComment = await con.queryAsync("INSERT INTO response_comment (user_id,content,course_comment_id,created_at) VALUES (?,?,?,?)",[user_id,text,course_comment_id,created_at]);
+  res.json('有了');
+});
+
 module.exports = router;
