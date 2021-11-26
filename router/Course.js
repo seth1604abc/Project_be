bf7499facda2e604c83740837058fb6f5dfa655a
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-11-26 21:21:58 +0800

    2021/11/26 - 留言板

--- router/Course.js
+++ router/Course.js
@@ -41,7 +41,7 @@ router.get("/SingleCourse/:id", async (req, res) => {
 });
 
 router.get("/comment", async (req, res) => {
-  let mainComment = await con.queryAsync("SELECT course_comment.*, user.first_name AS user_id ,user.image FROM course_comment JOIN user ON course_comment.user_id=user.id");
+  let mainComment = await con.queryAsync("SELECT course_comment.*, user.first_name AS user_id ,user.image FROM course_comment JOIN user ON course_comment.user_id=user.id ORDER BY id DESC");
   let sonComment = await con.queryAsync("SELECT response_comment.*, user.first_name AS user_id ,user.image FROM response_comment JOIN user ON response_comment.user_id=user.id");
   let allComment = [...mainComment,sonComment]
   res.json(allComment);
@@ -54,7 +54,7 @@ router.post("/addComment", async (req, res) => {
   let created_at =req.body.created_at
   console.log(req.body)
   let addComment = await con.queryAsync("INSERT INTO course_comment (user_id,content,course_id,created_at) VALUES (?,?,?,?)",[user_id,text,course_id,created_at]);
-  res.json('有了');
+  res.json('主留言有了');
 });
 
 router.post("/addChildrenComment", async (req, res) => {
@@ -64,7 +64,12 @@ router.post("/addChildrenComment", async (req, res) => {
   let created_at =req.body.created_at
   console.log(req.body)
   let addComment = await con.queryAsync("INSERT INTO response_comment (user_id,content,course_comment_id,created_at) VALUES (?,?,?,?)",[user_id,text,course_comment_id,created_at]);
-  res.json('有了');
+  res.json('副留言有了');
+});
+
+router.get("/hitCourse", async (req, res) => {
+  let hitCourse = await con.queryAsync("SELECT * FROM course ORDER BY likes DESC");
+  res.json(hitCourse);
 });
 
 module.exports = router;
