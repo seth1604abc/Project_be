b161a085d03fe00e396b2de23046a4a55082419a
hungchihsueh <chihsuehhung@gmail.com>
2021-11-27 15:10:58 +0800

    Update Cart.js

--- router/Cart.js
+++ router/Cart.js
@@ -4,22 +4,41 @@ const router = express.Router();
 
 //加入購物車
 router.post("/addcart/:productId/", async (req, res) => {
-    let id = req.session.userId;
-    let data = req.body;
-    console.log(data);   
-    let result = await con.queryAsync(
-      "INSERT INTO cart(user_id,product_id,amount) VALUES(?)",[[1,req.params.productId,data.number]]
-    );
-    res.json(result);
-  });
+  let id = req.session.userId;
+  let data = req.body;
+  // console.log(data);
+  let result = await con.queryAsync(
+    "INSERT INTO cart(user_id,product_id,amount) VALUES(?)",
+    [[1, req.params.productId, data.number]]
+  );
+  res.json(result);
+});
 router.get("/list", async (req, res) => {
-    let id = req.session.userId;
-    let result = await con.queryAsync(
-      "SELECT * FROM cart INNER JOIN product ON cart.product_id=product.id INNER JOIN product_images ON product.id=product_images.product_id WHERE is_main=1 AND user_id=1"
-    );
-    res.json(result);
-  });
+  let id = req.session.userId;
+  let result = await con.queryAsync(
+    "SELECT * FROM cart INNER JOIN product ON cart.product_id=product.id INNER JOIN product_images ON product.id=product_images.product_id WHERE is_main=1 AND user_id=1"
+  );
+  res.json(result);
+});
 
+router.delete("/delete/:productId", async (req, res) => {
+  let id = req.session.userId;
+  let result = await con.queryAsync(
+    "DELETE FROM cart WHERE product_id=? AND user_id=1",
+    [req.params.productId]
+  );
+  res.json(result);
+});
+router.delete("/delete-selected", async (req, res) => {
+  let id = req.session.userId;
+  let data = req.body.items;
+  console.log(typeof data); 
+  console.log(data);
 
+  let result = await con.queryAsync(
+    `DELETE FROM cart WHERE product_id IN (${data}) AND user_id=1`
+  );
+  res.json(result);
+});
 
-  module.exports = router;
+module.exports = router;
